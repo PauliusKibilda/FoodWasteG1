74bf23e8defad04cbd32337c7e76cb1a02f0f233
Aidas Virbalis <aidasmu@gmail.com>
2021-09-20 20:55:05 +0300

    Added basic Restaurant and Product classes

---
+++ FoodWaste/FoodWaste/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodWaste
+{
+    class Product
+    {
+        public string ProductName;
+        public enum ProductState
+        {
+            expired,
+            taken,
+            reserved,
+            listed
+        }
+        public ProductState State {get; set;}
+        public void SetProduct(string pName, ProductState pState)
+        {
+            
+            ProductName = pName;
+            State = pState;
+        }
+
+        public string GetProduct()
+        {
+            return ProductName;
+        }
+    }
+}
