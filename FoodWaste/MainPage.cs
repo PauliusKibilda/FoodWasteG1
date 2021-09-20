f1f17ad04f458f327b9a6d8c1a57488e0f6976fc
Paulius Kibilda <paulius.kibilda@proit.lt>
2021-09-20 21:36:01 +0300

    Added main page

---
+++ FoodWaste/MainPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodWaste
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //dataGridView1.CurrentRow.S
+        }
+    }
+}
