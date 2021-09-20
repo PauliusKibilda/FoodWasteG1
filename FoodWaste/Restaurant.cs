2b1d9001d70ed3123cf1083f039b02648a38ace3
Aidas Virbalis <aidasmu@gmail.com>
2021-09-20 21:13:03 +0300

    Added basic product and restaurant classes

---
+++ FoodWaste/Restaurant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodWaste
+{
+    class Restaurant
+    {
+        public string RestaurantName;
+        public string Adress;
+        public string PhoneNumber;
+
+        public void SetRestaurant(string pName, string pAdress, string pNumber)
+        {
+            RestaurantName = pName;
+            Adress = pAdress;
+            PhoneNumber = pNumber;
+        }
+
+        public string GetRestaurant()
+        {
+            return RestaurantName;
+        }
+    }
+}
