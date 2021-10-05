cd8289c3735db2b1b100a2fe7d9bd323da7f46b3
Vaidas Čibiras <55850111+siturez@users.noreply.github.com>
2021-10-05 21:47:22 +0300

    Added "Continue as guest" button

--- FoodWaste/LoginPage.cs
+++ FoodWaste/LoginPage.cs
@@ -33,9 +33,7 @@ namespace FoodWaste
         {
             if (CheckLoginCredentials(usernameTextBox.Text, passwordTextBox.Text))
             {
-                this.Hide();
-                MainPage mainPage = new MainPage();
-                mainPage.ShowDialog();
+                OpenMainPage();
             }
             else
             {
@@ -48,9 +46,9 @@ namespace FoodWaste
             passwordTextBox.UseSystemPasswordChar = !showPasswordCheckbox.Checked;
         }
 
-        private void BackButton_Click(object sender, EventArgs e)
+        private void ContinueAsGuestButton_Click(object sender, EventArgs e)
         {
-            // Go back to the main page
+            OpenMainPage();
         }
 
         private void signupButton_Click(object sender, EventArgs e)
@@ -63,6 +61,13 @@ namespace FoodWaste
         private void LoginPage_FormClosed(object sender, FormClosedEventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private void OpenMainPage()
+        {
+            this.Hide();
+            MainPage mainPage = new MainPage();
+            mainPage.ShowDialog();
         }
     }
 }
