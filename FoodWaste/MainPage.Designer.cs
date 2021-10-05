9517485ae3438ba15bb33c9468f6764b5bc35a0b
Vaidas Čibiras <55850111+siturez@users.noreply.github.com>
2021-10-05 22:27:33 +0300

    Renamed MainPage and RegisterOptions window names

--- FoodWaste/MainPage.Designer.cs
+++ FoodWaste/MainPage.Designer.cs
@@ -30,7 +30,7 @@ namespace FoodWaste
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
             this.MainDataGridView = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -75,8 +75,8 @@ namespace FoodWaste
             // Column2
             // 
             this.Column2.DataPropertyName = "ExpiryDate";
-            dataGridViewCellStyle11.Format = "yyyy-MM-dd";
-            this.Column2.DefaultCellStyle = dataGridViewCellStyle11;
+            dataGridViewCellStyle12.Format = "yyyy-MM-dd";
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle12;
             this.Column2.HeaderText = "Expiration date";
             this.Column2.Name = "Column2";
             this.Column2.ReadOnly = true;
@@ -183,7 +183,7 @@ namespace FoodWaste
             this.Controls.Add(this.MainDataGridView);
             this.MinimumSize = new System.Drawing.Size(721, 521);
             this.Name = "MainPage";
-            this.Text = "Main page";
+            this.Text = "Products";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainPage_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.MainDataGridView)).EndInit();
             this.gridClick.ResumeLayout(false);
