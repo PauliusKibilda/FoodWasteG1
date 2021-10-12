72c3a985f5aacc9fe4f5a045c50615c6f26c4444
Mindaugas <Mindaugas@LAPTOP-04UF3JJ4>
2021-10-12 12:23:08 -0700

    Updated Validation Rule For Duplicates Usernames. ChangedThings That Vaidas Commented.

--- FoodWaste/RegisterPageValidation.cs
+++ FoodWaste/RegisterPageValidation.cs
@@ -9,22 +9,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Mail;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace FoodWaste
 {
-   
     class RegisterPageValidation
     {
-
-
         private static string errorMessage;
         public static string getErrorMessage ()
         { 
             return errorMessage; 
         }
-
     public static bool ValidateUserName(string username)
-     {
+    {
             string usernameTrimmed = Regex.Replace(username, @"\s", "");
 
             if (String.IsNullOrWhiteSpace(username))
@@ -37,88 +34,78 @@ namespace FoodWaste
                 errorMessage = "Username Must Have At Least 4 Characters";
                 return false;
             }
-
             else if (usernameTrimmed.Length != username.Length)
             {
                 errorMessage = "Invalid Characters In Username";
                 return false;
             }
-            return true;
-        }
-
-        public static bool ValidateEmailAddress(string email)
-        {
-                try
-                {
-                    new System.Net.Mail.MailAddress(email);
-                }
-                catch (ArgumentNullException)
-                {
-                    errorMessage = "Email Address Can Not Be Empty";
-                    return false;
-                }
-                catch (ArgumentException)
-                {
-                    errorMessage = "Email Address Can Not Be Empty";
-                    return false;
-                }
-                catch (FormatException)
-                {
-                    errorMessage = "Invalid Format Of Email Address";
-                    return false;
-                }
-
-                return true;
-        }
-
-        public static bool ValidatePassword(string password)
-        {
-            if (String.IsNullOrWhiteSpace(password))
-            {
-                errorMessage = "Password Can Not Be Empty";
-                return false;
-            }
             else
             {
-                var hasNumber = new Regex(@"[0-9]+");
-                var hasUpperChar = new Regex(@"[A-Z]+");
-                var hasMinimum8Chars = new Regex(@".{8,}");
-                bool isValidated;
-
-                if (!(isValidated = hasMinimum8Chars.IsMatch(password)))
+                string text = System.IO.File.ReadAllText(@"C:\Users\Mindaugas\source\repos\2.0\FoodWaste\Accounts.txt");     
+                string[] values = text.Split(new char[] { ',', '\n' });
+                for (int i = 0; i < values.Length; i++)
                 {
-                    errorMessage = "Password Should Contain 8 Characters";
-                    return false;
-
-                }
-                else if (!(isValidated = hasUpperChar.IsMatch(password)))
-                {
-                    errorMessage = "Password Should Contain 1 Upper Case Letter";
-                    return false;
-
-                }
-                else if (!(isValidated = hasNumber.IsMatch(password)))
-                {
-                    errorMessage = "Password Should Contain One Number";
-                    return false;
-
-                }
-
+                    if (values[i].Equals(username))
+                    {
+                        errorMessage = "This Username Already Exists";
+                        return false;
+                    }
+                }     
                 return true;
-
             }
-
-        }
-        public static bool IsPasswordSame(string firstPassword, string secondPassword)
-        {
-            if(!firstPassword.Equals(secondPassword))
+    }
+    public static bool ValidateEmailAddress(string email)
+    {
+            try
             {
-                errorMessage = "Passwords Do Not Match";
+                new System.Net.Mail.MailAddress(email);
+            }
+            catch 
+            {
+                errorMessage = "Invalid Format Of Email Address";
                 return false;
             }
-
+            return true;
+    }
+    public static bool ValidatePassword(string password)
+    {
+        if (String.IsNullOrWhiteSpace(password))
+        {
+            errorMessage = "Password Can Not Be Empty";
+            return false;
+        }
+        else
+        {
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMinimum8Chars = new Regex(@".{8,}");
+            bool isValidated;
+            if (!(isValidated = hasMinimum8Chars.IsMatch(password)))
+            {
+                errorMessage = "Password Must Contain 8 Characters";
+                return false;
+            }
+            else if (!(isValidated = hasUpperChar.IsMatch(password)))
+            {
+                errorMessage = "Password Must Contain 1 Upper Case Letter";
+                return false;
+            }
+            else if (!(isValidated = hasNumber.IsMatch(password)))
+            {
+                errorMessage = "Password Must Contain One Number";
+                return false;
+            }
             return true;
         }
     }
-    
+    public static bool IsPasswordSame(string firstPassword, string secondPassword)
+    {
+        if(!firstPassword.Equals(secondPassword))
+        {
+            errorMessage = "Passwords Do Not Match";
+            return false;
+        }
+        return true;
+    }
+}
 }
